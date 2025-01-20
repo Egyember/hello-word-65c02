3287d774c683e33c3cc42921d9bbc68a63bdf147
Egy_ember <kamufiok06@gmail.com>
2025-01-21 00:02:43 +0100

    broken shift register but woring falling edge detection

--- main.asm
+++ main.asm
@@ -2,6 +2,7 @@ PORTB = $6000
 PORTA = $6001
 DDRB = $6002
 DDRA = $6003
+VIAT2L = $6008
 VIASFC = $600A
 VIAAUX = $600B
 VIACONT = $600C
@@ -16,6 +17,9 @@ DISPLAYMASK =  %11100000
 SETMODE   = %00111000
 DISPLAYON = %00001111
 CLEAR     = %00000001
+
+t2counter = $0001
+
 	.org $8000
 reset:
 	sei ;disable interrupts
@@ -25,14 +29,15 @@ reset:
 	sta DDRB ;set output mode on PORTB
 	lda #$ff 
 	sta DDRA ;set output mode on PORTA
-	lda #%11001100
+	lda #%00101100
 	sta VIACONT ;set controll reg
 	lda #%00000000
 	sta VIAAUX ;set aux controll reg
-	lda #%10010000
-	sta VIAIRQ ;disable all irq
-	cli
+	lda #%10001000
+	sta VIAIRQ ;disable all unused irq
 	jsr lcdinit
+	jsr lcdClear
+	cli
 
 main:
 	jsr lcdClear
@@ -59,7 +64,7 @@ bad$
 	lda #%00000010
 endif$
 	sta PORTA
-:	jmp :-
+	jmp end
 
 sendinst:
 	pha
@@ -177,32 +182,67 @@ longsleep:
 
 irq:
 	pha
-	phx
-	lda IRQFLAG
-	tax			;store a in x so I can check other bits
-	and #%00010000
-	cmp #%00010000
-	beq write$
+	lda #%00100000
+	and IRQFLAG
+	bne resetT2$
+endt2$
 
-	jmp end$
+	lda #%00001000
+	and IRQFLAG
+	bne uartstart$
+enduartst$
 
-write$
-	lda #%00010000
-	sta IRQFLAG
-	jsr lcdClear
-	ldx #$0
-loop$
-	lda gomb,x
-	cmp #'\0'
-	beq irq
-	jsr senddata
-	jsr waitBusy
-	inx
-	jmp loop$
-end$
-	plx
+	lda #%00000100
+	and IRQFLAG
+	bne uartdone$
+enduartdn$
+
+endirq$
 	pla
 	rti ;if interrupt happen return from it
+
+resetT2$
+	lda #'b'
+	jsr senddata
+	jsr waitBusy
+	lda t2counter
+	cmp #0
+	beq endt2$
+	sbc #1
+	sta t2counter
+	lda VIAT2L
+	adc #104 ;close enugh for 1MHz 104,166666667 is the accutate for 1MHz
+	sta VIAT2L
+	jmp endt2$
+
+uartstart$
+	lda #%10100100
+	sta VIAIRQ ;enable shift register, t2 irq and disable falling edge irq
+	lda #%00001000 ;clear irq
+	sta IRQFLAG
+	lda #8
+	sta t2counter
+	lda #104 ;close enugh for 1MHz 104,166666667 is the accutate for 1MHz
+	sta VIAT2L
+	lda #%00000100 ;Shift in under control of T2
+	sta VIAAUX
+	jmp enduartst$
+
+
+uartdone$
+	lda #%00000100
+	sta IRQFLAG ;clear irq
+	lda #%10001000
+	sta VIAIRQ ;reset setings irq
+	lda VIASFC
+	lda #'a'
+	jsr senddata
+	jsr waitBusy
+	lda #%00000000 ;Shift off
+	sta VIAAUX
+
+	jmp enduartdn$
+
 
 kezdo:
 	.ascii "2025_01_09\0"
